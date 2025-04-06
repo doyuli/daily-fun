41655b79379506378e8388a25e2986c96f09cf48
Yu Lia <88418949+doyuli@users.noreply.github.com>
2025-04-06 11:33:58 +0800

    fix：useResetableRef default value

--- packages/func/useResetableRef/index.ts
+++ packages/func/useResetableRef/index.ts
@@ -24,7 +24,7 @@ export function useResetableRefFn<T>(valueFn: () => T) {
  */
 export function useResetableRef<T>(value: T, clone = cloneDeep) {
   const _value = clone(value)
-  const state = ref(_value)
+  const state = ref(value)
 
   const reset = () => {
     state.value = clone(_value)
@@ -42,7 +42,7 @@ export function useResetableRef<T>(value: T, clone = cloneDeep) {
 export function useResetableReactive<T extends object>(value: T, clone = cloneDeep) {
   const _value = clone(value)
 
-  const state = reactive(_value)
+  const state = reactive(value)
 
   const reset = () => {
     Object.keys(state).forEach((key) => delete (state as any)[key])
