d3c5d405090d55da47296fb79d14376e95cbfd90
dyl <1938793150@qq.com>
2025-04-08 11:25:41 +0800

    feat

--- packages/shared/utils/constants.ts
+++ packages/shared/utils/constants.ts
@@ -1,18 +1,3 @@
 export const NOOP = () => {}
 
 export const promise = Promise.resolve()
-
-export function promiseTimeout<T>(
-  cb: (resolve: (value: T) => void, reject: (reason: any) => void) => void,
-  ms: number,
-) {
-  return new Promise<T>((resolve, reject) => {
-    setTimeout(() => {
-      cb(resolve, reject)
-    }, ms)
-  })
-}
-
-export function promiseResolve<T>(value: T, ms: number) {
-  return new Promise<T>((resolve) => setTimeout(() => resolve(value), ms))
-}
--- packages/shared/utils/util.ts
+++ packages/shared/utils/util.ts
@@ -1,3 +1,18 @@
+export function promiseTimeout<T>(
+  cb: (resolve: (value: T) => void, reject: (reason: any) => void) => void,
+  ms: number = 30,
+) {
+  return new Promise<T>((resolve, reject) => {
+    setTimeout(() => {
+      cb(resolve, reject)
+    }, ms)
+  })
+}
+
+export function promiseResolve<T>(value: T, ms: number = 30) {
+  return new Promise<T>((resolve) => setTimeout(() => resolve(value), ms))
+}
+
 /**
  * 深拷贝 使用 JSON.parse(JSON.stringify(value))
  * @param value 需要深拷贝的值
