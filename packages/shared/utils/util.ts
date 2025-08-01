c0ed575f870e33abe610e8254f4e4082f6981819
Yu Lia <1938793150@qq.com>
2025-08-01 22:11:56 +0800

    feat: jsonStringify

--- packages/shared/utils/util.ts
+++ packages/shared/utils/util.ts
@@ -3,5 +3,5 @@ export function cloneDeep<T>(val: T): T {
     return val
   return JSON.parse(JSON.stringify(val))
 }
-export const stringify = (val: unknown): string => JSON.stringify(val)
+export const jsonStringify = (val: unknown): string => JSON.stringify(val)
 export const promiseResolve = <T>(value: T, ms: number = 30) => new Promise<T>(resolve => setTimeout(() => resolve(value), ms))
