996b85f654bc994e3f36d039ae6020165ebc2626
Yu Lia <1938793150@qq.com>
2025-08-03 23:17:13 +0800

    feat: dependency ant-design-vue

--- playground/src/pages/async-component/index.vue
+++ playground/src/pages/async-component/index.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
+import type { Component } from 'vue'
 import { defineAsyncComponent } from '@daily-fun/components'
-import { h } from 'vue'
+import { Input } from 'ant-design-vue'
+import { h, shallowRef } from 'vue'
 
-const AsyncComponent = defineAsyncComponent({
+const input = shallowRef('async')
+
+function loadComponent() {
+  return new Promise<Component>((resolve) => {
+    setTimeout(() => {
+      resolve(Input)
+    }, 1000)
+  })
+}
+
+const AsyncInput = defineAsyncComponent({
   loadingComponent: () => h('span', 'loading'),
   errorComponent: () => h('span', 'error'),
-  loader: () => Promise.resolve(() => h('span', 'I am async component')),
+  loader: loadComponent,
 })
 </script>
 
 <template>
   <div>
     <h1>AsyncComponent</h1>
-    <AsyncComponent />
+    <div>
+      <p>
+        asyncInput <AsyncInput v-model:value="input" class="w-300px" />
+      </p>
+      <p>asyncValue: {{ input }}</p>
+    </div>
   </div>
 </template>
 
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import UnoCSS from 'unocss/vite'
-import ElementPlus from 'unplugin-element-plus/vite'
 import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -14,13 +13,9 @@ export default defineConfig({
     VueRouter(),
     // ⚠️ Vue must be placed after VueRouter()
     vue(),
-    UnoCSS(),
     vueJsx(),
-    ElementPlus({
-      format: 'esm',
-      useSource: true,
-    }),
     vueDevTools(),
+    UnoCSS(),
   ],
   resolve: {
     alias: {
